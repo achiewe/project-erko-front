a4d69b370abe988e01cb675886c571fac1125fbd
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 19:37:08 +0400

    add the height

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -8,13 +8,13 @@ import Image from "next/image";
 
 export default function LoadingContainer() {
   return (
-    <div className="flex flex-col overflow-hidden w-full h-full justify-start items-center gap-[40px] bg-[#FFFFF0] border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px]">
+    <div className="flex flex-col overflow-hidden w-full h-[600px] justify-start items-center gap-[40px] bg-[#FFFFF0] border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px]">
       <h1 className="text-[24px] sm:text-[32px] text-center">
         Welcome to Erko
       </h1>
       <GifButtonDiv />
       <CreativityTxtDiv />
-      <Image src={anyDeskPng} alt="any desk png" className="fixed bottom-20" />
+      <Image src={anyDeskPng} alt="any desk png" className="fixed bottom-10" />
     </div>
   );
 }
