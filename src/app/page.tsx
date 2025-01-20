1a4719d11e2f8ffddd9ea1f1063dfc67582eca29
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 16:16:15 +0400

    remove the padding top on the main element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import PageList from "./components/pageList/pageList";
 export default function Home() {
   return (
     <main className="flex flex-col w-full h-screen bg-[#EEEEEE] justify-center items-center">
-      <main className="flex px-[15px] pt-[15px] w-full">
+      <main className="flex px-[15px] w-full">
         <PageList />
         <LoadingContainer />
       </main>
