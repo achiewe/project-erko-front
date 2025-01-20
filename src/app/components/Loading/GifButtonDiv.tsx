c3a8b939d7d87fc2c63c814281e9b60940b256cb
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 20:10:32 +0400

    change the gif sizes

--- src/app/components/Loading/GifButtonDiv.tsx
+++ src/app/components/Loading/GifButtonDiv.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 function GifButtonDiv() {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-[10px]">
-      <Image src={LoadingGif} alt="loading gif" width={200} height={200} />
+      <Image src={LoadingGif} alt="loading gif" width={150} height={150} />
       <button className="bg-[#FFFFF0] border-r-2 border-b-2 border-gray-500 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] border-solid flex justify-center items-center w-[85px] h-[24px] cursor-pointer">
         START
       </button>
