5b560a153e2d43ac4ac949e09975e654ba0afa17
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 19:42:36 +0400

    add items start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import LoadingContainer from "./components/Loading/LoadingContainer";
 export default function Home() {
   return (
     <main className="flex flex-col w-full h-screen bg-[#EEEEEE]">
-      <div className="flex px-[15px] w-full h-full py-[15px] items-center justify-center">
+      <div className="flex px-[15px] w-full h-full py-[15px] items-start">
         <LoadingContainer />
       </div>
     </main>
