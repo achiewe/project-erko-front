db186113ff519c3d1540c9c385c16fac4f1c739b
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 20:00:21 +0400

    add container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import LoadingContainer from "./components/Loading/LoadingContainer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full h-screen bg-[#EEEEEE]">
-      <div className="flex px-[15px] w-full h-full py-[80px] items-start">
-        <LoadingContainer />
-      </div>
+    <main className="flex flex-col w-full h-screen bg-[#EEEEEE] py-[30px] px-[15px]">
+      <LoadingContainer />
     </main>
   );
 }
