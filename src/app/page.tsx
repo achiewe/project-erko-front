747d9dae77f6acde579360621de955620f070705
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 19:23:27 +0400

    change the height and add some of the styles oferflow hidder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
 "use client";
-import { useEffect } from "react";
 import LoadingContainer from "./components/Loading/LoadingContainer";
-import PageList from "./components/pageList/pageList";
 
 export default function Home() {
-  useEffect(() => {
-    // Lock scrolling by setting the body's overflow to hidden
-    document.body.style.overflow = "hidden";
-
-    return () => {
-      // Re-enable scrolling when the component unmounts
-      document.body.style.overflow = "";
-    };
-  }, []);
-
   return (
-    <main className="flex flex-col w-full min-h-screen bg-[#EEEEEE]">
-      <div className="flex px-[15px] w-full min-h-screen py-[15px]">
-        <PageList />
+    <main className="flex flex-col w-full h-screen bg-[#EEEEEE]">
+      <div className="flex px-[15px] w-full h-full py-[15px]">
         <LoadingContainer />
       </div>
     </main>
