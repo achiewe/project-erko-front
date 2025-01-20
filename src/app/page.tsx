4619e4bb3d6c3e14a26dd9fac0af187b9ebe9fd9
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 16:48:29 +0400

    change the padding top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import PageList from "./components/pageList/pageList";
 export default function Home() {
   return (
     <main className="flex flex-col w-full h-screen bg-[#EEEEEE]">
-      <main className="flex px-[15px] pt-[60px] w-full">
+      <main className="flex px-[15px] pt-[30px] w-full">
         <PageList />
         <LoadingContainer />
       </main>
