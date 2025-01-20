c435ee6f7635a4058cb39d3072a3c2a809fc4c0a
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 20:22:13 +0400

    create main erko page ruting and also write on the start button click and when it is happend should route and appear erko main page

--- src/app/components/Loading/GifButtonDiv.tsx
+++ src/app/components/Loading/GifButtonDiv.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import LoadingGif from "../../../../public/assets/video.gif";
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { setIsLoading } from "@/app/redux/features/IsLoadingSlice";
 
 function GifButtonDiv() {
+  const dispatch = useDispatch();
   return (
     <div className="flex flex-col w-full justify-center items-center gap-[10px]">
       <Image src={LoadingGif} alt="loading gif" width={150} height={150} />
-      <button className="bg-[#FFFFF0] border-r-2 border-b-2 border-gray-500 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] border-solid flex justify-center items-center w-[85px] h-[24px] cursor-pointer">
+      <button
+        onClick={() => {
+          dispatch(setIsLoading(true));
+        }}
+        className="bg-[#FFFFF0] border-r-2 border-b-2 border-gray-500 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] border-solid flex justify-center items-center w-[85px] h-[24px] cursor-pointer"
+      >
         START
       </button>
     </div>
