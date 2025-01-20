8f95f6b744cb98f2ddc0a869a23ca064b5d838a8
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 16:08:07 +0400

    update LoadingContainer style

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -8,11 +8,17 @@ import Image from "next/image";
 
 export default function LoadingContainer() {
   return (
-    <div className="flex bg-[#FFFFF0] border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] py-[40px] flex-col w-full min-h-full justify-start items-center gap-[60px]">
-      <h1 className="text-[32px]">Welcome to Erko</h1>
+    <div className="flex flex-col w-full min-h-full justify-center items-center gap-[40px] bg-[#FFFFF0] border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] p-4 sm:p-6">
+      <h1 className="text-[24px] sm:text-[32px] text-center">
+        Welcome to Erko
+      </h1>
       <GifButtonDiv />
       <CreativityTxtDiv />
-      <Image src={anyDeskPng} alt="any desk png" />
+      <Image
+        src={anyDeskPng}
+        alt="any desk png"
+        className="max-w-[90%] h-auto"
+      />
     </div>
   );
 }
