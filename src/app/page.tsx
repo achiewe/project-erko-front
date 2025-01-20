a8b23fafd308cfe2b3a9015b63d51e8cfdccd5c2
achiewe <mamukashviliachi706@gmail.com>
2025-01-20 20:05:30 +0400

    remove border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import LoadingContainer from "./components/Loading/LoadingContainer";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full h-screen bg-[#EEEEEE] py-[30px] px-[15px]">
-      <LoadingContainer />
+    <main className="flex flex-col w-full h-screen bg-[#EEEEEE]">
+      <div className="flex px-[15px] w-full h-full items-start">
+        <LoadingContainer />
+      </div>
     </main>
   );
 }
