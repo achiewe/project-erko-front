2bb03d2ad6b79ec00f147e978122527d9c6aaf94
achiewe <mamukashviliachi706@gmail.com>
2025-01-22 20:02:39 +0400

    add in the header erko logo

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import erkoLogo from "../../../../public/assets/erkoLogo.png";
 export default function Header() {
   return (
     <header className="w-full bg-white py-[10px] border-b-[2px] border-black shadow-lg">
-      <nav className="w-full flex flex-row justify-center items-center gap-[15px]">
+      <nav className="w-full flex flex-row justify-start items-center gap-[20px] pl-[15px]">
+        <Image src={erkoLogo} alt="erko logo" />
         <button className="relative underline-button text-[20px]">File</button>
         <button className="relative underline-button text-[20px]">View</button>
         <button className="relative underline-button text-[20px]">
