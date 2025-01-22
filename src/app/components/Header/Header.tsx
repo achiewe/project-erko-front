1414ccf141e2c06c1618a7aceae7cc96e3898cee
achiewe <mamukashviliachi706@gmail.com>
2025-01-22 19:58:39 +0400

    add border bottom on the header and do on them some drop down shadow

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 export default function Header() {
   return (
-    <header className="w-full bg-white py-[10px]">
+    <header className="w-full bg-white py-[10px] border-b-[2px] border-black shadow-lg">
       <nav className="w-full flex flex-row justify-center items-center gap-[15px]">
-        <button className="relative underline-button">File</button>
-        <button className="relative underline-button">View</button>
-        <button className="relative underline-button">Surprise</button>
-        <button className="relative underline-button">Help</button>
+        <button className="relative underline-button text-[20px]">File</button>
+        <button className="relative underline-button text-[20px]">View</button>
+        <button className="relative underline-button text-[20px]">
+          Surprise
+        </button>
+        <button className="relative underline-button text-[20px]">Help</button>
       </nav>
     </header>
   );
