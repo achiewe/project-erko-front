4b5e2dbcc48b55fb6568161477631afd409e66ab
achiewe <mamukashviliachi706@gmail.com>
2025-01-22 22:11:07 +0400

    importMenuItem component inside the DropDownMenu component

--- src/app/components/Header/DropDownMenu.tsx
+++ src/app/components/Header/DropDownMenu.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MenuItem from "./MenuItem";
 
 export default function DropDownMenu() {
   return (
@@ -7,21 +8,11 @@ export default function DropDownMenu() {
       aria-labelledby="file-menu-button"
       className="absolute pl-[7px] top-[20px] bg-[white] w-[160px] border-b-2 border-r-2 border-l-2 border-t-2 border-b-gray-800 border-r-gray-800 border-l-gray-200 border-t-gray-200"
     >
-      <li role="menuitem" className="hover:bg-gray-100">
-        <button type="button">New File</button>
-      </li>
-      <li role="menuitem" className="hover:bg-gray-100">
-        <button type="button">Open</button>
-      </li>
-      <li role="menuitem" className="">
-        <button type="button">Print</button>
-      </li>
-      <li role="menuitem" className="text-[#bbbbbb]">
-        <button type="button">Close Window</button>
-      </li>
-      <li role="menuitem" className="text-[#bbbbbb]">
-        <button type="button">Search</button>
-      </li>
+      <MenuItem text="New File" />
+      <MenuItem text="Open" />
+      <MenuItem text="Print" />
+      <MenuItem text="Close Window" disabled />
+      <MenuItem text="Search" disabled />
     </ul>
   );
 }
