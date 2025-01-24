1dc54f436479256233687331c319a047f4ae887b
achiewe <mamukashviliachi706@gmail.com>
2025-01-24 20:42:08 +0400

    add on the anydesk image position absolute

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -10,7 +10,7 @@ export default function LoadingContainer() {
   return (
     <div
       className="flex flex-col w-full h-full h-sm:gap-[15px] h-sm:h-[570px] h-mm:h-[500px] justify-start items-center gap-[40px] bg-[#FFFFF0] 
-    border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px]"
+    border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px] relative"
     >
       <h1 className="text-[36px] font-bold sm:text-[32px] text-center h-sm:text-[24px]">
         Welcome to Erko
@@ -20,7 +20,7 @@ export default function LoadingContainer() {
       <Image
         src={anyDeskPng}
         alt="any desk png"
-        className="fixed bottom-10 h-sm:bottom-20"
+        className="absolute bottom-10 h-sm:bottom-20"
       />
     </div>
   );
