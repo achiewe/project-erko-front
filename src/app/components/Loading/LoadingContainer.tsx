ab005ca3cae3ce1d9052c0d2405e5cf2b96f02ad
achiewe <mamukashviliachi706@gmail.com>
2025-01-24 16:47:40 +0400

    change the height od the loadingContainer when will change the height of device

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function LoadingContainer() {
   return (
     <div
-      className="flex flex-col w-full h-full h-sm:gap-[5px] justify-start items-center gap-[40px] bg-[#FFFFF0] 
+      className="flex flex-col w-full h-full h-sm:gap-[5px] h-sm:h-[450px] justify-start items-center gap-[40px] bg-[#FFFFF0] 
     border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px]"
     >
       <h1 className="text-[36px] font-bold sm:text-[32px] text-center h-sm:text-[24px]">
