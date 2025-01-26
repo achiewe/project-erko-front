0d6bcad20d5a41a9afafc6094971de5835581e36
achiewe <mamukashviliachi706@gmail.com>
2025-01-26 14:38:02 +0400

    add the logic on the ul element when show this element and when hide

--- src/app/components/Header/DropDownMenu.tsx
+++ src/app/components/Header/DropDownMenu.tsx
@@ -1,12 +1,20 @@
+"use client";
 import React from "react";
 import MenuItem from "./MenuItem";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/features/store";
 
 export default function DropDownMenu() {
+  const isDropDown = useSelector(
+    (store: RootState) => store.isDropDown.isDropDown
+  );
   return (
     <ul
       role="menu"
       aria-labelledby="file-menu-button"
-      className={`absolute pl-[7px] top-[28px] left-[55px] bg-[white] w-[160px] border-b-2 border-r-2 border-l-2 border-t-2 border-b-gray-800 border-r-gray-800 border-l-gray-200 border-t-gray-200`}
+      className={`${
+        isDropDown ? "hidden" : "absolute"
+      } pl-[7px] top-[28px] left-[55px] bg-[white] w-[160px] border-b-2 border-r-2 border-l-2 border-t-2 border-b-gray-800 border-r-gray-800 border-l-gray-200 border-t-gray-200`}
     >
       <MenuItem text="New File" />
       <MenuItem text="Open" />
