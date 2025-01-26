f26c984ba981f3031f35c5ec63cdfeebf1a94796
achiewe <mamukashviliachi706@gmail.com>
2025-01-26 17:53:22 +0400

    change the height and width when height decrease

--- src/app/components/ErkoContainer/OptionCardErko.tsx
+++ src/app/components/ErkoContainer/OptionCardErko.tsx
@@ -18,7 +18,11 @@ export default function OptionCardErko({
   return (
     <div className="w-[120px] flex items-center flex-col justify-center">
       <Link href={link}>
-        <Image src={imageSrc} alt={title} className="h-[120px]" />
+        <Image
+          src={imageSrc}
+          alt={title}
+          className="h-[120px] h-sm:h-[90px] h-sm:w-[100px]"
+        />
         <h3 className="text-center text-[18px]">{title}</h3>
       </Link>
     </div>
