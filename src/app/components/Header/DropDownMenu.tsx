100a1e4c18d50ca802cc3ba11d8d8433fe6f06eb
achiewe <mamukashviliachi706@gmail.com>
2025-01-27 17:20:40 +0400

    change the favicon

--- src/app/components/Header/DropDownMenu.tsx
+++ src/app/components/Header/DropDownMenu.tsx
@@ -14,7 +14,8 @@ export default function DropDownMenu() {
       aria-labelledby="file-menu-button"
       className={`${
         isDropDown ? "hidden" : "absolute"
-      } pl-[7px] top-[28px] left-[55px] z-10 bg-[white] w-[160px] border-b-2 border-r-2 border-l-2 border-t-2 border-b-gray-800 border-r-gray-800 border-l-gray-200 border-t-gray-200`}
+      } pl-[7px] top-[28px] left-[55px] z-10 bg-[white] w-[160px] border-b-2 border-r-2 border-l-2 border-t-2 
+      border-b-gray-800 border-r-gray-800 border-l-gray-200 border-t-gray-200`}
     >
       <MenuItem text="New File" />
       <MenuItem text="Open" />
