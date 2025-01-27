72df8df1f9e3c76e62f95afcdb55a85a9dd93887
achiewe <mamukashviliachi706@gmail.com>
2025-01-27 21:04:22 +0400

    change the height size on the image and padding size on the div

--- src/app/components/ErkoContainer/MainErkoContainer.tsx
+++ src/app/components/ErkoContainer/MainErkoContainer.tsx
@@ -12,8 +12,9 @@ import archivePng from "../../../../public/assets/archive.png";
 export default function MainErkoContainer() {
   return (
     <div
-      className="flex mt-[1px] flex-row items-center justify-center w-full h-screen gap-y-[20px] h-mm:gap-y-[5px] flex-wrap gap-x-[50px] bg-[#FFFFF0] 
-    border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] py-[20px] relative"
+      className="flex mt-[1px] flex-row items-center justify-center w-full h-screen 
+      gap-y-[20px] h-mm:gap-y-[5px] h-mn:gap-y-[0px] flex-wrap gap-x-[50px] bg-[#FFFFF0] 
+    border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] py-[20px] h-mn:py-[0px] relative"
     >
       <OptionCardErko title="Team" imageSrc={teamPng} link="/erko/team" />
       <OptionCardErko
