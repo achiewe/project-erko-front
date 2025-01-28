a0d81aa64b9cea855ddf1e41523908fa0170a698
achiewe <mamukashviliachi706@gmail.com>
2025-01-28 14:51:06 +0400

    add on the Link element flexbox styles

--- src/app/components/ErkoContainer/OptionCardErko.tsx
+++ src/app/components/ErkoContainer/OptionCardErko.tsx
@@ -16,12 +16,12 @@ export default function OptionCardErko({
   link,
 }: OptionCardProps) {
   return (
-    <div className="w-[150px] flex items-center flex-col justify-center">
-      <Link href={link}>
+    <div className="w-[150px]">
+      <Link href={link} className="flex justify-center items-center flex-col">
         <Image
           src={imageSrc}
           alt={title}
-          className="h-[150px] w-[170px] ml-auto h-sm:h-[120px] h-sm:w-[140px] h-mm:h-[120px] h-mm:w-[140px] h-mn:h-[110px] h-mn:w-[130px] 
+          className="h-[150px] w-[170px] h-sm:h-[120px] h-sm:w-[140px] h-mm:h-[120px] h-mm:w-[140px] h-mn:h-[110px] h-mn:w-[130px] 
           h-ml:h-[60px] h-ml:w-[80px]"
         />
         <h3 className="text-center text-[20px]">{title}</h3>
