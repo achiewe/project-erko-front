7aca583edb447a3c4b72f407f374b7d0748aac96
achiewe <mamukashviliachi706@gmail.com>
2025-01-30 21:10:00 +0400

    create new component inside the FormComponents file FormButton and import it on the form page

--- src/app/components/FormComponents/FormRadio.tsx
+++ src/app/components/FormComponents/FormRadio.tsx
@@ -1,58 +1,58 @@
-import { FC } from "react";
-import {
-  FieldValues,
-  UseFormRegister,
-  FieldError,
-  UseFormSetValue,
-  UseFormWatch,
-} from "react-hook-form";
+// import { FC } from "react";
+// import {
+//   FieldValues,
+//   UseFormRegister,
+//   FieldError,
+//   UseFormSetValue,
+//   UseFormWatch,
+// } from "react-hook-form";
 
-interface RadioGroupProps {
-  label: string;
-  name: string;
-  options: { value: string; label: string }[];
-  register: UseFormRegister<FieldValues>;
-  setValue: UseFormSetValue<FieldValues>;
-  watch: UseFormWatch<FieldValues>;
-  errors: FieldError | undefined;
-}
+// interface RadioGroupProps {
+//   label: string;
+//   name: string;
+//   options: { value: string; label: string }[];
+//   register: UseFormRegister<FieldValues>;
+//   setValue: UseFormSetValue<FieldValues>;
+//   watch: UseFormWatch<FieldValues>;
+//   errors: FieldError | undefined;
+// }
 
-const FormRadio: FC<RadioGroupProps> = ({
-  label,
-  name,
-  options,
-  register,
-  setValue,
-  watch,
-  errors,
-}) => {
-  return (
-    <div>
-      <label className="block font-medium">{label}</label>
-      <div className="flex items-center space-x-4">
-        {options.map((option) => (
-          <label key={option.value} className="flex items-center space-x-2">
-            <input
-              type="radio"
-              value={option.value}
-              {...register(name)}
-              className="hidden"
-              onChange={() => setValue(name, option.value)}
-            />
-            <span
-              className={`w-6 h-6 border border-gray-400 flex items-center justify-center ${
-                watch(name) === option.value ? "bg-gray-900 text-white" : ""
-              }`}
-            >
-              {watch(name) === option.value ? "X" : ""}
-            </span>
-            <span>{option.label}</span>
-          </label>
-        ))}
-      </div>
-      <p className="text-red-500 text-sm">{errors?.message}</p>
-    </div>
-  );
-};
+// const FormRadio: FC<RadioGroupProps> = ({
+//   label,
+//   name,
+//   options,
+//   register,
+//   setValue,
+//   watch,
+//   errors,
+// }) => {
+//   return (
+//     <div>
+//       <label className="block font-medium">{label}</label>
+//       <div className="flex items-center space-x-4">
+//         {options.map((option) => (
+//           <label key={option.value} className="flex items-center space-x-2">
+//             <input
+//               type="radio"
+//               value={option.value}
+//               {...register(name)}
+//               className="hidden"
+//               onChange={() => setValue(name, option.value)}
+//             />
+//             <span
+//               className={`w-6 h-6 border border-gray-400 flex items-center justify-center ${
+//                 watch(name) === option.value ? "bg-gray-900 text-white" : ""
+//               }`}
+//             >
+//               {watch(name) === option.value ? "X" : ""}
+//             </span>
+//             <span>{option.label}</span>
+//           </label>
+//         ))}
+//       </div>
+//       <p className="text-red-500 text-sm">{errors?.message}</p>
+//     </div>
+//   );
+// };
 
-export default FormRadio;
+// export default FormRadio;
--- src/app/jobs/form/page.tsx
+++ src/app/jobs/form/page.tsx
@@ -1,3 +1,4 @@
+import FormButton from "@/app/components/FormComponents/FormButton";
 import FormPage from "@/app/components/FormComponents/FormPage";
 import Header from "@/app/components/Header/Header";
 import React from "react";
@@ -6,10 +7,12 @@ export default function page() {
   return (
     <div>
       <Header />
-      <div className="flex px-[5px] w-full h-full items-start pb-[10px] flex-col">
+      <div className="flex px-[5px] w-full h-full items-end pb-[10px] flex-col">
+        <FormButton />
         <div
-          className="flex flex-row items-start sm:justify-center justify-start w-full h-full h-mn:gap-y-[0px] h-sm:gap-y-[10px] h-mm:pt-[0px] gap-y-[40px] flex-wrap gap-x-[50px] bg-[#FFFFF0] 
-border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] py-[20px] relative"
+          className="flex flex-row items-start sm:justify-center justify-start w-full 
+          h-full h-mn:gap-y-[0px] h-sm:gap-y-[10px] h-mm:pt-[0px] gap-y-[40px] flex-wrap gap-x-[50px] bg-[#FFFFF0] 
+          border-r-2 border-b-2 border-gray-800 drop-shadow-[0px_0px_3px_rgba(0,0,0,0.40)] py-[20px] relative"
         >
           <FormPage />
         </div>
