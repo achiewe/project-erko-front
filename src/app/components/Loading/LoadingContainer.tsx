667690a643bce7c780857690e0ecf5ca446ec38f
achiewe <mamukashviliachi706@gmail.com>
2025-01-31 14:38:29 +0400

    remvoe the gap from the component style

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function LoadingContainer() {
   return (
     <div
-      className="flex flex-col overflow-hidden w-full h-full h-sm:gap-[30px] h-sv:h-[660px] 
+      className="flex flex-col overflow-hidden w-full h-full h-sv:h-[660px] 
       h-sm:h-[730px] h-mm:h-[610px] h-mn:h-[570px] h-mw:h-[540px] h-ml:h-[490px] justify-start items-center gap-[40px] bg-[#FFFFF0] 
     border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px] relative"
     >
