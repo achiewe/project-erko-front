35723e333008c11731a95a265e78b3e352903523
achiewe <mamukashviliachi706@gmail.com>
2025-01-31 14:27:50 +0400

    grow size h1 tag text

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -13,7 +13,7 @@ export default function LoadingContainer() {
       h-sm:h-[730px] h-mm:h-[610px] h-mn:h-[570px] h-mw:h-[540px] h-ml:h-[490px] justify-start items-center gap-[40px] bg-[#FFFFF0] 
     border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px] relative"
     >
-      <h1 className="text-[36px] font-bold sm:text-[32px] text-center h-sm:text-[24px]">
+      <h1 className="text-[36px] font-bold sm:text-[32px] text-center">
         Welcome to Erko
       </h1>
       <GifButtonDiv />
