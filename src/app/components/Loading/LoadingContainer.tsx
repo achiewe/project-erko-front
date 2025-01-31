c5fdd3f9bc6c4fa51032d846f496473da706747e
achiewe <mamukashviliachi706@gmail.com>
2025-01-31 14:51:01 +0400

    grow gap size and add on the anyDesk png bottom

--- src/app/components/Loading/LoadingContainer.tsx
+++ src/app/components/Loading/LoadingContainer.tsx
@@ -10,7 +10,7 @@ export default function LoadingContainer() {
   return (
     <div
       className="flex flex-col overflow-hidden w-full h-full h-sv:h-[660px] 
-      h-sm:h-[730px] h-mm:h-[610px] h-mn:h-[570px] h-mw:h-[540px] h-ml:h-[490px] justify-start items-center gap-[40px] bg-[#FFFFF0] 
+      h-sm:h-[730px] h-mm:h-[610px] h-mn:h-[570px] h-mw:h-[540px] h-ml:h-[490px] justify-start items-center gap-[50px] bg-[#FFFFF0] 
     border-r-2 border-b-2 border-gray-800 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] pt-[30px] relative"
     >
       <h1 className="text-[36px] font-bold sm:text-[32px] text-center">
@@ -21,7 +21,7 @@ export default function LoadingContainer() {
       <Image
         src={anyDeskPng}
         alt="any desk png"
-        className="absolute bottom-10 h-sm:bottom-20"
+        className="absolute bottom-10 h-sm:bottom-5"
         width={131}
         height={24}
       />
