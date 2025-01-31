a9b9982fde0ab42b51838fcbea3f6eebf6caaa09
achiewe <mamukashviliachi706@gmail.com>
2025-01-31 15:10:02 +0400

    grow the button text size

--- src/app/components/Loading/GifButtonDiv.tsx
+++ src/app/components/Loading/GifButtonDiv.tsx
@@ -19,7 +19,7 @@ function GifButtonDiv() {
           dispatch(setIsLoading(true));
           router.push("/erko");
         }}
-        className="bg-[#FFFFF0] border-r-2 border-b-2 border-gray-500 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] 
+        className="bg-[#FFFFF0] text-[20px] border-r-2 border-b-2 border-gray-500 drop-shadow-[4px_3px_3px_rgba(0,0,0,0.40)] 
         focus:drop-shadow-[4px_3px_3px_rgba(0,0,0,0.10)] border-solid flex justify-center items-center w-[90px] h-[30px] cursor-pointer"
       >
         START
