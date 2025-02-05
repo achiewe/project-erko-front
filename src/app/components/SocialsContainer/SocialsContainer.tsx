30a8915df2989a7df0e9f5dc71896ad2beb25fc1
achiewe <mamukashviliachi706@gmail.com>
2025-02-05 14:51:08 +0400

    remove youtube link from the component

--- src/app/components/SocialsContainer/SocialsContainer.tsx
+++ src/app/components/SocialsContainer/SocialsContainer.tsx
@@ -3,15 +3,14 @@ import SocialsInfo from "./SocialsInfo";
 import faceBookLink from "../../../../public/assets/facebookLink.png";
 import instagramLink from "../../../../public/assets/instagramLink.png";
 import tiktokLink from "../../../../public/assets/tiktokLink.png";
-import youtubeLink from "../../../../public/assets/youtube.png";
 
 export default function SocialsContainer() {
   return (
     <div
-      className="flex flex-col gap-y-[80px] w-full items-center justify-sta min-h-screen bg-[#FFFFF0] 
+      className="flex flex-col gap-y-[80px] w-full items-center justify-start min-h-screen bg-[#FFFFF0] 
       border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)] py-[40px] relative"
     >
-      <div className="flex justify-between gap-x-[55px]">
+      <div className="flex items-start justify-between gap-x-[55px]">
         <SocialsInfo
           profileUrl="https://www.facebook.com/ErkoAgency"
           imageSrc={faceBookLink}
@@ -23,16 +22,11 @@ export default function SocialsContainer() {
           title="Instagram"
         />
       </div>
-      <div className="flex justify-between gap-x-[55px]">
+      <div className="flex justify-between gap-x-[55px] min-w-[229px]">
         <SocialsInfo
           profileUrl="https://www.tiktok.com/@erko.ge"
           imageSrc={tiktokLink}
           title="TikTok"
-        />
-        <SocialsInfo
-          profileUrl="https://www.facebook.com/ErkoAgency"
-          imageSrc={youtubeLink}
-          title="YouTube"
         />
       </div>
     </div>
