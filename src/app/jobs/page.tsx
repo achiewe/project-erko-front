01c53f0dc52539740f4f645d1a2cfe2267cf3580
achiewe <mamukashviliachi706@gmail.com>
2025-02-05 23:09:48 +0400

    add background color on the div

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -5,7 +5,7 @@ import JobsMainContainer from "../components/JobsContainer/JobsMainContainer";
 
 export default function page() {
   return (
-    <div>
+    <div className="bg-[#EEEEEE]">
       <Header />
       <div className="flex px-[5px] w-full h-full items-start pb-[10px] flex-col">
         <NavigationPages />
