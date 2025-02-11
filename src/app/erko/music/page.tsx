4d658eb6fce6ead7a5f1922f1502857cb6cc0ab9
achiewe <mamukashviliachi706@gmail.com>
2025-02-11 18:00:08 +0400

    create music div and add inside of it header compoennt

--- src/app/erko/music/page.tsx
+++ src/app/erko/music/page.tsx
@@ -1,5 +1,10 @@
+import Header from "@/app/components/Header/Header";
 import React from "react";
 
 export default function page() {
-  return <div> music</div>;
+  return (
+    <div className="w-full h-full bg-[#EEEEEE]">
+      <Header />
+    </div>
+  );
 }
