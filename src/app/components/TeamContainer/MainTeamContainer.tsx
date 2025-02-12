9feb96db35be346fec994b084b51991b5d5c4b5e
achiewe <mamukashviliachi706@gmail.com>
2025-02-12 19:20:31 +0400

    import other team members profile pngs and update the MainTeamContainer inside the TeamCOntainer folder and create also other team members detail pages

--- src/app/components/TeamContainer/MainTeamContainer.tsx
+++ src/app/components/TeamContainer/MainTeamContainer.tsx
@@ -3,6 +3,10 @@ import TeamCardErko from "./TeamCardErko";
 import LukaPxl from "../../../../public/assets/luka pixel .png";
 import TemikoPxl from "../../../../public/assets/Temiko pixel .png";
 import AchiPxl from "../../../../public/assets/Achi pixelman .png";
+import VaxanaPxl from "../../../../public/assets/pixelan vaxana .png";
+import DachiPxl from "../../../../public/assets/dachi pixelman .png";
+import SandroPxl from "../../../../public/assets/Sandro pixelman .png";
+import NikaPxl from "../../../../public/assets/nika pixeled.png";
 export default function MainTeamContainer() {
   return (
     <div
@@ -12,9 +16,10 @@ export default function MainTeamContainer() {
       <TeamCardErko title="Luka" imageSrc={LukaPxl} link="/erko/team/1" />
       <TeamCardErko title="Temiko" imageSrc={TemikoPxl} link="/erko/team/2" />
       <TeamCardErko title="Achi" imageSrc={AchiPxl} link="/erko/team/3" />
-      <TeamCardErko title="Dachi" imageSrc={LukaPxl} link="/erko/team/4" />
-      <TeamCardErko title="Sandro" imageSrc={LukaPxl} link="/erko/team/5" />
-      <TeamCardErko title="Luka" imageSrc={LukaPxl} link="/erko/team/6" />
+      <TeamCardErko title="Luka" imageSrc={VaxanaPxl} link="/erko/team/4" />
+      <TeamCardErko title="Dachi" imageSrc={DachiPxl} link="/erko/team/5" />
+      <TeamCardErko title="Sandro" imageSrc={SandroPxl} link="/erko/team/6" />
+      <TeamCardErko title="Nika" imageSrc={NikaPxl} link="/erko/team/7" />
     </div>
   );
 }
