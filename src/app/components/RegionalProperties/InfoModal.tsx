39b0d7dce79da7be973b11c30f288298da7e062e
achiewe <mamukashviliachi706@gmail.com>
2025-02-16 20:25:25 +0400

    add logic when show the infoModal and when hide it

--- src/app/components/RegionalProperties/InfoModal.tsx
+++ src/app/components/RegionalProperties/InfoModal.tsx
@@ -1,11 +1,19 @@
+"use client";
 import React from "react";
 import ModalButton from "./ModalButton";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/features/store";
 
 export default function InfoModal() {
+  const openModal = useSelector(
+    (store: RootState) => store.openModal.openModal
+  );
   return (
     <div
-      className="w-[233px] bg-[#F3F3F3] flex flex-col p-[5px] absolute top-[150px]  gap-[10px] justify-center items-center 
-    border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)]"
+      className={`w-[233px] bg-[#F3F3F3] ${
+        openModal ? "flex" : "hidden"
+      }  flex-col p-[5px] absolute top-[200px] gap-[10px] justify-center items-center 
+    border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)]`}
     >
       <p className="text-center text-[18px]">
         ERKO supports international projects, adapting and restructuring our
