7e9c7804fc92dabeaa141a1bbbc77a4c0eab4c1b
achiewe <mamukashviliachi706@gmail.com>
2025-02-16 16:49:46 +0400

    create regionalProperties component where will be main regions

---
+++ src/app/components/RegionalProperties/MainRegionalContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MainRegionalContainer() {
+  return (
+    <div
+      className="flex flex-col gap-y-[80px] w-full items-center justify-start min-h-screen bg-[#FFFFF0] 
+  border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)] py-[40px] relative"
+    ></div>
+  );
+}
--- src/app/erko/regional/page.tsx
+++ src/app/erko/regional/page.tsx
@@ -1,5 +1,16 @@
+import Header from "@/app/components/Header/Header";
+import BackButton from "@/app/components/MemberCard/BackButton";
+import MainRegionalContainer from "@/app/components/RegionalProperties/MainRegionalContainer";
 import React from "react";
 
 export default function page() {
-  return <div> regional</div>;
+  return (
+    <div className="w-full h-full bg-[#EEEEEE]">
+      <Header />
+      <div className="flex px-[5px] w-full h-full items-start flex-col pb-[10px]">
+        <BackButton destination="/erko" />
+        <MainRegionalContainer />
+      </div>
+    </div>
+  );
 }
