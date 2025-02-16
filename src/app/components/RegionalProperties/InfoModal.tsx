21ce45605ccdc4fe93def5f9ed0accca05e11ab5
achiewe <mamukashviliachi706@gmail.com>
2025-02-16 20:08:08 +0400

    create new component infoModal where is the information about erko locations

---
+++ src/app/components/RegionalProperties/InfoModal.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function InfoModal() {
+  return (
+    <div className="w-[233px] h-[267px] bg-[#F3F3F3] flex flex-col p-[10px] absolute gap-[10px] justify-center items-center">
+      <p className="text-center text-[18px]">
+        ERKO supports international projects, adapting and restructuring our
+        teams to ensure the success of each project.
+      </p>
+      <p className="text-center text-[18px]">
+        The red dots on the map represent international locations where our
+        teams are currently operating remotely.
+      </p>
+    </div>
+  );
+}
--- src/app/components/RegionalProperties/MainRegionalContainer.tsx
+++ src/app/components/RegionalProperties/MainRegionalContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import RegionalMap from "./RegionalMap";
+import InfoModal from "./InfoModal";
 
 export default function MainRegionalContainer() {
   return (
@@ -8,6 +9,7 @@ export default function MainRegionalContainer() {
   border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)]"
     >
       <RegionalMap />
+      <InfoModal />
     </div>
   );
 }
