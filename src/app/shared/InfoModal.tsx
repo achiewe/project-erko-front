6ae9c4914018d36c0de070be583dfa1a9bce1538
achiewe <mamukashviliachi706@gmail.com>
2025-02-22 21:56:24 +0400

    add display logic inside style property

--- src/app/shared/InfoModal.tsx
+++ src/app/shared/InfoModal.tsx
@@ -15,10 +15,11 @@ export default function InfoModal({ txt1, txt2 }: infoModalProps) {
   );
   return (
     <div
-      className={`w-[233px] bg-[#F3F3F3] ${
-        openModal ? "flex" : "hidden"
-      }  flex-col px-[8px] py-[20px] top-[-300px] h-fit left-0 right-[0] bottom-0 gap-[10px] justify-center items-center 
+      className={`w-[233px] bg-[#F3F3F3] flex-col px-[8px] py-[20px] top-[-300px] h-fit left-0 right-[0] bottom-0 gap-[10px] justify-center items-center 
     border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)] m-auto absolute z-10`}
+      style={{
+        display: openModal ? "flex" : "none",
+      }}
     >
       <p className="text-center text-[18px]">{txt1}</p>
       <p className="text-center text-[18px]">{txt2}</p>
