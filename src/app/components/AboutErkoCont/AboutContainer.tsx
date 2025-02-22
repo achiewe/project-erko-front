ac5b3374e9f49b328113d660c1197bacc9caec5f
achiewe <mamukashviliachi706@gmail.com>
2025-02-22 16:46:33 +0400

    Add About section navigation with Erko files

--- src/app/components/AboutErkoCont/AboutContainer.tsx
+++ src/app/components/AboutErkoCont/AboutContainer.tsx
@@ -7,14 +7,17 @@ export default function AboutContainer() {
       className="flex flex-row flex-wrap w-full items-start justify-start min-h-screen bg-[#FFFFF0] 
 border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)] relative"
     >
-      <AggementFiles navigate="/erko" aggementName="Founders Story" />
       <AggementFiles
-        navigate="/erko"
+        navigate="about/founder-story"
+        aggementName="Founder's Story"
+      />
+      <AggementFiles
+        navigate="about/core-principles"
         aggementName="Core Principles [MVV]"
         textWidth="150px"
         bottom="-39px"
       />
-      <AggementFiles navigate="/erko" aggementName="Our Team" />
+      <AggementFiles navigate="about/team" aggementName="Our Team" />
     </div>
   );
 }
