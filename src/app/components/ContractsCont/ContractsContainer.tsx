cb18014c8482ed46797f7f786750de3dac1a0b82
achiewe <mamukashviliachi706@gmail.com>
2025-02-22 16:53:43 +0400

    add navigation for contracts section and contract files

--- src/app/components/ContractsCont/ContractsContainer.tsx
+++ src/app/components/ContractsCont/ContractsContainer.tsx
@@ -7,10 +7,16 @@ export default function ContractsContainer() {
       className="flex flex-row flex-wrap w-full items-start justify-start min-h-screen bg-[#FFFFF0] 
 border-r-2 border-b-2 border-gray-800 drop-shadow-[1px_1px_3px_rgba(0,0,0,0.40)] relative"
     >
-      <ContractFile navigate="/erko" fileName="One Month (trial)" />
-      <ContractFile navigate="/erko" fileName="Full Package" />
-      <ContractFile navigate="/erko" fileName="Ads Sprint" />
-      <ContractFile navigate="/erko" fileName="Content Plan" />
+      <ContractFile
+        navigate="contracts/one-month-trial"
+        fileName="One Month (trial)"
+      />
+      <ContractFile navigate="contracts/full-package" fileName="Full Package" />
+      <ContractFile
+        navigate="contracts/ads-sprint-plan"
+        fileName="Ads Sprint"
+      />
+      <ContractFile navigate="contracts/content-plan" fileName="Content Plan" />
     </div>
   );
 }
---
+++ src/app/erko/documents/contracts/content-plan/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/Header/Header";
+import BackButton from "@/app/components/MemberCard/BackButton";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="w-full h-full bg-[#EEEEEE]">
+      <Header />
+      <div className="flex px-[5px] w-full h-full items-start flex-col pb-[10px]">
+        <BackButton destination="/erko/documents/contracts" />
+      </div>
+    </div>
+  );
+}
