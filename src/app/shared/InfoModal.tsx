855ede26c9c3bb3e857c9365f457f55332e92ac3
achiewe <mamukashviliachi706@gmail.com>
2025-02-23 15:17:34 +0400

    change the size of the infomodal div and p tag text size

--- src/app/shared/InfoModal.tsx
+++ src/app/shared/InfoModal.tsx
@@ -15,15 +15,15 @@ export default function InfoModal({ txt1, txt2 }: infoModalProps) {
   );
   return (
     <div
-      className="w-[233px] bg-[#F3F3F3] flex-col px-[8px] py-[20px] top-[-300px] h-fit left-0 right-[0] bottom-0 gap-[10px] justify-center items-center 
+      className="max-w-[300px] bg-[#F3F3F3] flex-col px-[8px] py-[20px] top-[-300px] h-fit left-0 right-[0] bottom-0 gap-[30px] justify-center items-center 
       border-r-2 border-b-2 border-gray-800 m-auto absolute z-10"
       style={{
         display: openModal ? "flex" : "none",
         boxShadow: openModal ? "1px 1px 3px rgba(0, 0, 0, 0.40)" : "none", // Applying the shadow conditionally
       }}
     >
-      <p className="text-center text-[18px]">{txt1}</p>
-      <p className="text-center text-[18px]">{txt2}</p>
+      <p className="text-center text-[24px]">{txt1}</p>
+      <p className="text-center text-[24px]">{txt2}</p>
       <ModalButton />
     </div>
   );
