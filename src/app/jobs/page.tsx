6e8f94d5fe8bd4219b6ac43bb2e7a112ae775d93
achiewe <mamukashviliachi706@gmail.com>
2025-03-02 00:42:26 +0400

    add responsive styles for jobs page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "../components/Header/Header";
 import NavigationPages from "../components/Navigation/NavigationPages";
 import JobsMainContainer from "../components/JobsContainer/JobsMainContainer";
+import Wrapper from "../shared/Wrapper";
 
 export default function page() {
   return (
-    <div className="bg-[#EEEEEE]">
-      <Header />
-      <div className="flex px-[5px] w-full h-full items-start pb-[10px] flex-col">
-        <NavigationPages />
-        <JobsMainContainer />
+    <Wrapper>
+      <div className="bg-[#EEEEEE] lg:max-w-[960px] lg:max-h-[700px]">
+        <Header />
+        <div className="flex px-[5px] w-full h-full items-start pb-[10px] flex-col lg:px-[20px]">
+          <NavigationPages />
+          <JobsMainContainer />
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 }
