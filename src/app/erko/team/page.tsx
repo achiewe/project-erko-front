3d43ac5c65381a6057ab0135f5c2f479f7884b2c
achiewe <mamukashviliachi706@gmail.com>
2025-03-02 19:52:45 +0400

    style team erko card

--- src/app/erko/team/page.tsx
+++ src/app/erko/team/page.tsx
@@ -1,16 +1,19 @@
 import Header from "@/app/components/Header/Header";
 import NavigationPages from "@/app/components/Navigation/NavigationPages";
 import MainTeamContainer from "@/app/components/TeamContainer/MainTeamContainer";
+import Wrapper from "@/app/shared/Wrapper";
 import React from "react";
 
 export default function page() {
   return (
-    <div className="w-full min-h-screen bg-[#EEEEEE]">
-      <Header />
-      <div className="flex px-[5px] w-full h-full items-start flex-col pb-[10px]">
-        <NavigationPages />
-        <MainTeamContainer />
+    <Wrapper>
+      <div className="w-full min-h-screen bg-[#EEEEEE] lg:max-w-[960px] lg:min-h-[697px] lg:shadow-lg lg:shadow-gray-800">
+        <Header />
+        <div className="flex px-[5px] w-full h-full items-start flex-col pb-[10px] lg:px-[20px]">
+          <NavigationPages />
+          <MainTeamContainer />
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 }
