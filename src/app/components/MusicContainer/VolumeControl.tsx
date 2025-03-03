3a238e3f4202c0d322f7ce5d81d7a6d8f7c7b9db
achiewe <mamukashviliachi706@gmail.com>
2025-03-03 18:50:43 +0400

    update volumeControl component

--- src/app/components/MusicContainer/VolumeControl.tsx
+++ src/app/components/MusicContainer/VolumeControl.tsx
@@ -1,40 +1,52 @@
 "use client";
 
 import { VolumeControlProps } from "../../../../type/type";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function VolumeControl({
   volume,
   changeVolume,
 }: VolumeControlProps) {
-  // Calculate the initial gradient style based on the initial volume
-  const initialGradient = `linear-gradient(to right, #858585 ${
-    volume * 100
-  }%, #DBDBDB ${volume * 100}%)`;
+  const sliderRef = useRef<HTMLInputElement>(null);
 
-  // Update the gradient style whenever the volume changes
   useEffect(() => {
-    const slider = document.getElementById("volume-slider") as HTMLInputElement;
+    const slider = sliderRef.current;
     if (slider) {
       slider.style.setProperty("--volume-percent", `${volume * 100}%`);
     }
   }, [volume]);
 
+  // Handler for change events (mouse and keyboard)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeVolume(parseFloat(e.target.value));
+  };
+
+  // Handler for touch events (mobile devices)
+  const handleTouch = (e: React.TouchEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    changeVolume(parseFloat(target.value));
+  };
+
   return (
     <div className="w-full">
       <input
         type="range"
+        ref={sliderRef}
         id="volume-slider"
         min="0"
         max="1"
         step="0.01"
         value={volume}
-        onChange={(e) => changeVolume(parseFloat(e.target.value))}
+        onChange={handleChange} // For mouse and keyboard input
+        onTouchStart={handleTouch} // For touch input
+        onTouchMove={handleTouch} // For touch input
         className="volume-slider"
         style={{
           WebkitAppearance: "none",
           appearance: "none",
-          background: initialGradient,
+          background: `linear-gradient(to right, #858585 ${
+            volume * 100
+          }%, #DBDBDB ${volume * 100}%)`,
         }}
       />
     </div>
