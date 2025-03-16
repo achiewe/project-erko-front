29e8bbd647f5f5e58e2beacc7994fbad65c8d485
achiewe <mamukashviliachi706@gmail.com>
2025-03-16 17:07:52 +0400

    cahnge request

--- src/app/hooks/useSubmitHelpForm.ts
+++ src/app/hooks/useSubmitHelpForm.ts
@@ -25,7 +25,7 @@ export function useSubmitHelpForm() {
             }
           }
       
-          const response = await fetch(`https://project-erko-back-production.up.railway.app/help`, {
+          const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/help`, {
             method: "POST",
             body: data,
           });
