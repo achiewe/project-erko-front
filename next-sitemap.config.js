9391ce610304e856b255149a71e9c3fdfdf65ac1
achiewe <mamukashviliachi706@gmail.com>
2025-03-18 10:58:08 +0400

    add sitemap and robots files for seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.erko.ge", // Change this to your actual website URL
+  generateRobotsTxt: true, // Generate robots.txt automatically
+};
