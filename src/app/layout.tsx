b45b2310944ce1caf300f23878c0e9c52c0c6151
achiewe <mamukashviliachi706@gmail.com>
2025-04-15 16:42:47 +0400

    add verel analytics inside the layout tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ReduxProvider } from "./redux/provider";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -30,6 +31,7 @@ export default function RootLayout({
       </head>
       <body className={myfont.className}>
         <ReduxProvider>{children}</ReduxProvider>
+        <Analytics />
       </body>
     </html>
   );
